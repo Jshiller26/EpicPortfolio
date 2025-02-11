045729d60047a0acb12629c381dc921566b9e28e
Jshiller26 <shiller1205@gmail.com>
2025-02-11 14:23:16 -0500

    fixed centering on image and seperated subtitle text to 2 lines

--- epicportfolio/src/app/components/StartScreen.tsx
+++ epicportfolio/src/app/components/StartScreen.tsx
@@ -22,34 +22,55 @@ export default function StartScreen() {
           background: 'linear-gradient(135deg, #1a5c4a 0%, #2d8b6d 100%)',
         }}
       >
-    </div>
+      </div>
 
       {/* Main content */}
       <div className="relative flex flex-col items-center justify-center h-full">
         {/* Title Logo */}
-        <div className="title-container mb-4">
+        <div className="title-container mb-4 flex justify-center">
           <Image
-            src="/images/PokemonName2.png"
+            src="/images/pokemonName3.png"
             alt="Joe Shiller"
-            width={400}
-            height={150}
+            width={600}
+            height={200}
             priority
+            className="transform -translate-y-20" 
           />
         </div>
 
-        {/* Subtitle */}
-        <div 
-          className="text-white text-2xl mb-8"
-          style={{ fontFamily: 'BengalPixel, sans-serif' }}
-        >
-          SOFTWARE DEVELOPER
+        {/* Subtitle text */}
+        <div className="flex flex-col items-center -mt-8 mb-16">
+          <div 
+            className="text-white text-4xl tracking-wider"
+            style={{ 
+              fontFamily: 'BengalPixel, sans-serif',
+              transform: 'perspective(500px) rotateX(10deg)',
+              textShadow: '2px 2px 0px #000'
+            }}
+          >
+            SOFTWARE
+          </div>
+          <div 
+            className="text-white text-4xl tracking-wider mt-1"
+            style={{ 
+              fontFamily: 'BengalPixel, sans-serif',
+              transform: 'perspective(500px) rotateX(10deg)',
+              textShadow: '2px 2px 0px #000'
+            }}
+          >
+            DEVELOPER
+          </div>
         </div>
 
         {/* Press Start Text */}
         <div 
-          className={`press-start text-white text-2xl mt-8 transition-opacity duration-200 ${
+          className={`press-start text-white text-3xl tracking-wider transition-opacity duration-200 ${
             isBlinking ? 'opacity-100' : 'opacity-0'
           }`}
+          style={{ 
+            fontFamily: 'BengalPixel, sans-serif',
+            textShadow: '2px 2px 0px #000'
+          }}
         >
           PRESS START
         </div>
