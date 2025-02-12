59c14551e473f2aa474a3834f16735b624b10139
Jshiller26 <shiller1205@gmail.com>
2025-02-12 15:24:53 -0500

    can now get past the start screen

---
+++ epicportfolio/src/app/home/page.tsx
@@ -0,0 +1,17 @@
+// src/app/home/page.tsx
+"use client";
+
+import React from 'react';
+// import Room from '../components/Room';
+// import Player from '../components/Player';
+
+export default function Home() {
+  return (
+    <div className="relative w-full h-screen bg-black flex items-center justify-center">
+      <div className="relative w-[640px] h-[480px] overflow-hidden">
+        {/* <Room /> */}
+        {/* <Player /> */}
+      </div>
+    </div>
+  );
+}
