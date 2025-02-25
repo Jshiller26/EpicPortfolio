62da06145047102ae8df6348d8dc7792cf287e12
Jshiller26 <shiller1205@gmail.com>
2025-02-25 18:41:48 -0500

    added missing files to prevent crash

--- src/app/stores/fileSystem/initialState.ts
+++ src/app/stores/fileSystem/initialState.ts
@@ -1,4 +1,4 @@
-import { FileSystemState, Folder } from '../../types/fileSystem';
+import { FileSystemState, Folder, File } from '../../types/fileSystem';
 
 // Set up initial file system structure
 const initialState: FileSystemState = {
@@ -91,6 +91,91 @@ const initialState: FileSystemState = {
       created: new Date(),
       modified: new Date(),
       parentId: 'my-projects',
+      children: []
+    } as Folder,
+    
+    'readme': {
+      id: 'readme',
+      name: 'README.txt',
+      type: 'file',
+      extension: 'txt',
+      path: 'C:\\Desktop\\README.txt',
+      created: new Date(),
+      modified: new Date(),
+      parentId: 'desktop',
+      content: 'Hi',
+      size: 100
+    } as File,
+    
+    'resume': {
+      id: 'resume',
+      name: 'Resume.pdf',
+      type: 'file',
+      extension: 'pdf',
+      path: 'C:\\Desktop\\Documents\\Resume.pdf',
+      created: new Date(),
+      modified: new Date(),
+      parentId: 'documents',
+      content: '[PDF content would be here]',
+      size: 250
+    } as File,
+    
+    'download-file-1': {
+      id: 'download-file-1',
+      name: 'Interesting-Article.pdf',
+      type: 'file',
+      extension: 'pdf',
+      path: 'C:\\Desktop\\Downloads\\Interesting-Article.pdf',
+      created: new Date(),
+      modified: new Date(),
+      parentId: 'downloads',
+      content: '[PDF content would be here]',
+      size: 1024
+    } as File,
+    
+    'download-file-2': {
+      id: 'download-file-2',
+      name: 'Cool-Project.zip',
+      type: 'file',
+      extension: 'zip',
+      path: 'C:\\Desktop\\Downloads\\Cool-Project.zip',
+      created: new Date(),
+      modified: new Date(),
+      parentId: 'downloads',
+      content: '[ZIP binary content]',
+      size: 5120
+    } as File,
+    
+    'screenshots': {
+      id: 'screenshots',
+      name: 'Screenshots',
+      type: 'folder',
+      path: 'C:\\Desktop\\Pictures\\Screenshots',
+      created: new Date(),
+      modified: new Date(),
+      parentId: 'pictures',
+      children: []
+    } as Folder,
+    
+    'EMC-Photobooth': {
+      id: 'EMC-Photobooth',
+      name: 'EMC-Photobooth',
+      type: 'folder',
+      path: 'C:\\Desktop\\My Projects\\EMC\\EMC-Photobooth',
+      created: new Date(),
+      modified: new Date(),
+      parentId: 'emc',
+      children: []
+    } as Folder,
+    
+    'Emc-QuizGame': {
+      id: 'Emc-QuizGame',
+      name: 'Emc-QuizGame',
+      type: 'folder',
+      path: 'C:\\Desktop\\My Projects\\EMC\\Emc-QuizGame',
+      created: new Date(),
+      modified: new Date(),
+      parentId: 'emc',
       children: []
     } as Folder
   },
