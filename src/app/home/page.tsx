8ad715edd7685c4e638220c7caf53dd0652ba00f
Jshiller26 <shiller1205@gmail.com>
2025-02-26 14:31:11 -0500

    wrapped home page in a suspense boundary for vercel

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import Room from '../components/Room';
+
+function RoomComponent() {
+  return <Room />;
+}
 
 export default function Home() {
   return (
     <div className="w-full h-screen bg-black">
-      <Room />
+      <Suspense fallback={<div className="w-full h-screen bg-black flex items-center justify-center text-white">Loading...</div>}>
+        <RoomComponent />
+      </Suspense>
     </div>
   );
 }
